6a51c1284c4643cbee00d217d0de9878b875d4db
JohnVettas <johnvettas7@gmail.com>
2026-04-15 17:04:04 +0300

    scrapped previous idea starting from the start with a normal personal website

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+export default function BigNav() {
+    return(
+        <nav className="bigNav">
+            <ul>
+                <li><a href="#home" className="navItems-Left">Home</a></li>
+                <li><a href="#skills" className="navItems-Left">Skills</a></li>
+                <li><a href="#projects" className="navItems-Left">Projects</a></li>
+                <li><a href="#contact" className="navItems-Left">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
